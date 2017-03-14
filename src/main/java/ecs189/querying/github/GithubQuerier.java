4340af24f4a37b42bbfc796360b16431ec0cc716
jamiestarr1 <jastarr@ucdavis.edu>
2017-03-13 17:21:11 -0700

    should now have a commit message and first 8 of the SHA, listed in a vertical list numbered 1 -> w/e

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -18,35 +18,76 @@ import java.util.List;
 public class GithubQuerier {
 
     private static final String BASE_URL = "https://api.github.com/users/";
+    private static final String TOKEN = "#access_token=cbe55b6bd013e096925e134e0f70ede3803ac8c0";
 
     public static String eventsAsHTML(String user) throws IOException, ParseException {
         List<JSONObject> response = getEvents(user);
         StringBuilder sb = new StringBuilder();
         sb.append("<div>");
+
+        String pushType = "PushEvent";
+
         for (int i = 0; i < response.size(); i++) {
             JSONObject event = response.get(i);
-            // Get event type
-            String type = event.getString("type");
-            // Get created_at date, and format it in a more pleasant style
-            String creationDate = event.getString("created_at");
-            SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-            SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
-            Date date = inFormat.parse(creationDate);
-            String formatted = outFormat.format(date);
 
-            // Add type of event as header
-            sb.append("<h3 class=\"type\">");
-            sb.append(type);
-            sb.append("</h3>");
-            // Add formatted date
-            sb.append(" on ");
-            sb.append(formatted);
-            sb.append("<br />");
-            // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
-            sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
-            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
-            sb.append(event.toString());
-            sb.append("</pre> </div>");
+          if (pushType.equalsIgnoreCase(event.getString("type"))){
+                // Get event type
+                String type = event.getString("type");
+
+                // Get created_at date, and format it in a more pleasant style
+                String creationDate = event.getString("created_at");
+                SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+                SimpleDateFormat outFormat = new SimpleDateFormat("dd MMM, yyyy");
+                Date date = inFormat.parse(creationDate);
+                String formatted = outFormat.format(date);
+
+                // Add type of event as header
+                sb.append("<h3 class=\"type\">");
+                sb.append(type);
+                sb.append("</h3>");
+
+                // Add formatted date
+                sb.append(" on ");
+                sb.append(formatted);
+                sb.append("<br />");
+
+
+                // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
+                sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
+                sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
+                sb.append(event.toString());
+                sb.append("</pre> </div>");
+
+                //a vertically formatted list of all its commits with their SHA ( abbrev to first 8 characters)
+                // and their commit message
+                JSONObject payload = event.getJSONObject("payload");
+                if(payload.has("commits")){
+
+                    JSONArray commitArray;
+                    commitArray = payload.getJSONArray("commits");
+
+                    sb.append("<p>Commit and \"Message\" </p> ");
+
+                    for (int j = 0; j < commitArray.length(); j++) {
+
+
+                        JSONObject jsonobject = commitArray.getJSONObject(j);
+                        String sha = jsonobject.getString("sha");
+                        String message = jsonobject.getString("message");
+                        int counter = j+1;
+
+                        sb.append("<p>" + counter +") ");
+                        sb.append(sha.substring(0,8));
+                        sb.append(" \"" + message +"\"");
+                        sb.append("</p>");
+                    }
+                }
+
+
+
+          }
+
+
         }
         sb.append("</div>");
         return sb.toString();
@@ -54,7 +95,7 @@ public class GithubQuerier {
 
     private static List<JSONObject> getEvents(String user) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
-        String url = BASE_URL + user + "/events";
+        String url = BASE_URL + user + "/events" + TOKEN; //need to add token here
         System.out.println(url);
         JSONObject json = Util.queryAPI(new URL(url));
         System.out.println(json);
