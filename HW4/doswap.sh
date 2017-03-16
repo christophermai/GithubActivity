4b47188e5681e48a9914bbd500fdd92dcd28d9e7
christophermai <christopher.cm.mai@gmail.com>
2017-03-15 19:46:29 -0700

    added doswap.sh added README.SUBMISSION

---
+++ HW4/doswap.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+# This shell is to swap from outside the container
+# parameter of image name to run and swap into
+
+#50% extra credit
+#if $1=activity2
+if [ "$1" = 'activity2' ];
+ then
+ docker run -d --name=web2 --network=ecs189_default $1
+ sleep 10 && docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
+ sleep 10 && docker kill ecs189_web1_1
+ docker rm ecs189_web1_1
+fi
+
+#if $1=activity
+if [ "$1" = 'activity' ];
+ then
+ docker run -d --name=web1 --network=ecs189_default $1
+ sleep 10 && docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
+ sleep 10 && docker kill web2
+ docker rm web2
+fi
